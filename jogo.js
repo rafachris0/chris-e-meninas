dc836324f4ee53e64abb1b2d691c8773870176a9
christian <105943076+rafachris0@users.noreply.github.com>
2024-03-11 11:51:17 -0300

    ebewb

--- jogo.js
+++ jogo.js
@@ -3,9 +3,9 @@ let numero_Secreto = 13;
 let chute = prompt('Escolha um numero de 0 a 20');
 
 if(numero_Secreto == chute){
-alert('parabens, voce acertou!');
+alert('parabens, voce nao é um animal');
 }else{
-alert('que triste, voce errou :(');
+alert('tu é burro ein');
 
 }
 
