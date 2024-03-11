59357b705370ab0cd5d0b92aed494dc45f274df6
christian <105943076+rafachris0@users.noreply.github.com>
2024-03-11 11:50:04 -0300

    vad

--- jogo.js
+++ jogo.js
@@ -1,4 +1,13 @@
 let numero_Secreto = 13;
 
-prompt('Escolha um numero de 0 a 20');
+let chute = prompt('Escolha um numero de 0 a 20');
+
+if(numero_Secreto == chute){
+alert('parabens, voce acertou!');
+}else{
+alert('que triste, voce errou :(');
+
+}
+
+
 
