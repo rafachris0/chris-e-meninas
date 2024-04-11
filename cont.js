a86dd6bcbee158f3ba910446659af45d77c8845a
christian <105943076+rafachris0@users.noreply.github.com>
2024-04-11 08:55:59 -0300

    rayjr

--- cont.js
+++ cont.js
@@ -3,6 +3,7 @@ console.log(botoes.length)
 
 for(let i = 0; i<botoes.length; i++){
 console.log(i);
+botoes[i].onclick = function(){
 botoes[i].classList.add('ativo');
 //botoes[0].class.List.remove('ativo');
 }
