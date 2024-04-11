a88169b41352ac76bc9d73b78177c4d07ba45afb
christian <105943076+rafachris0@users.noreply.github.com>
2024-04-11 09:02:01 -0300

    ctyld

--- cont.js
+++ cont.js
@@ -4,6 +4,11 @@ console.log(botoes.length)
 for(let i = 0; i<botoes.length; i++){
 console.log(i);
 botoes[i].onclick = function(){
+    for(let j = 0; j<botoes.length; j++){
+        botoes[j].classList.remove('ativo')
+    }
+
+
 botoes[i].classList.add('ativo');
 //botoes[0].class.List.remove('ativo');
 }
