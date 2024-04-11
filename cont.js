524957fc1d4600c82582639e99aa82572beb3fca
christian <105943076+rafachris0@users.noreply.github.com>
2024-04-11 08:54:32 -0300

    aetjua4

--- cont.js
+++ cont.js
@@ -3,6 +3,6 @@ console.log(botoes.length)
 
 for(let i = 0; i<botoes.length; i++){
 console.log(i);
-botoes[1]classList.add('ativo');
+botoes[i].classList.add('ativo');
 //botoes[0].class.List.remove('ativo');
 }
